577177411e8041b3f5f1ddef2358b809abc50377
michaelalbert11 <53479113+michaelalbert11@users.noreply.github.com>
2022-08-13 17:34:23 +0530

    filter correction

--- src/components/Filter/Filter.component.jsx
+++ src/components/Filter/Filter.component.jsx
@@ -1,31 +1,77 @@
+import { useState } from "react";
 import "./Filter.style.scss";
+import { MdCircle, MdArrowDropDown } from "react-icons/md";
 export default function Filter(props) {
   const { handleOnClick } = props;
+  const [filterState, setFilterState] = useState({
+    selectedOption: "no filter",
+    optionActive: false,
+  });
+  function setCurrentOption(event) {
+    setFilterState((prevState) => ({
+      ...prevState,
+      selectedOption: event.target.innerText,
+    }));
+    event.stopPropagation();
+    console.log("option", filterState.selectedOption);
+  }
+
   return (
-    <select className="filter">
-      <option
-        onClick={() => {
-          handleOnClick("default");
-        }}
+    <div className="filter">
+      <button
+        className="filter__btn flex-centerd-btw"
+        onClick={() =>
+          setFilterState((prevState) => ({
+            ...prevState,
+            optionActive: !prevState.optionActive,
+          }))
+        }
+        onBlur={() =>
+          setFilterState((prevState) => ({ ...prevState, optionActive: false }))
+        }
       >
-        <div className="filter__option">no filter</div>
-      </option>
-      <option
-        className="filter__option"
-        onClick={() => {
-          handleOnClick("veg");
-        }}
-      >
-        veg
-      </option>
-      <option
-        className="filter__option"
-        onClick={() => {
-          handleOnClick("nonveg");
-        }}
-      >
-        non veg
-      </option>
-    </select>
+        <span>{filterState.selectedOption}</span>
+        <MdArrowDropDown className="filter__btn-icon" />
+      </button>
+      {filterState.optionActive && (
+        <ul className="filter__options">
+          <li
+            className="filter__option flex-centerd-btw"
+            onMouseDown={(event) => {
+              setCurrentOption(event);
+              handleOnClick("default");
+            }}
+          >
+            <span>no filter</span>
+          </li>
+          <li
+            className="filter__option flex-centerd-btw"
+            onMouseDown={(event) => {
+              setCurrentOption(event);
+              handleOnClick("veg");
+            }}
+          >
+            <span>veg</span>
+            <MdCircle
+              style={{ color: "green" }}
+              className="filter__option-icon"
+            />
+          </li>
+          <li
+            className="filter__option flex-centerd-btw"
+            onMouseDown={(event) => {
+              setCurrentOption(event);
+              handleOnClick("nonveg");
+            }}
+          >
+            <span>non veg</span>
+            <MdCircle
+              style={{ color: "red" }}
+              className="filter__option-icon"
+            />
+          </li>
+        </ul>
+      )}
+    </div>
   );
 }
